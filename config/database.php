450fe11e8f7e43da5c6599fcc9646204adde174b
Juan <juan04-bravo08@hotmail.com>
2025-03-28 18:33:30 -0500

    signup

--- config/database.php
+++ config/database.php
@@ -1,27 +1,27 @@
 <?php
-// config connection
-$host    = "localhost";
-$port    = "5432";
-$dbname   = "schoolar";
+
+//Config connection 
+$host      = "localhost";
+$port      = "5432";
+$dbname    = "schoolar";
 $user      = "postgres";
-$password   = "unicesmag";
+$password  = "unicesmag";
 
-// Create connetion
+//create connection
 $conn = pg_connect("
- host=$host
- port=$port  
-  dbname=$dbname
- user=$user 
- password=$password 
-
+host=$host
+port=$port
+dbname=$dbname
+user=$user
+password=$password
 ");
+
 if(!$conn){
-    die("connection error:". pg_last_error());
-  
+    die("Connection error: " . pg_last_error());
+}else{
+    echo "Success connection";
+} 
 
-    }else{ 
-        echo "success onnection";
-}
-  pg_close();
 
+// pg_close();
 ?>
--- src/signup.php
+++ src/signup.php
@@ -1,25 +1,36 @@
-<?
-include('../config/database.php'); // invocar 
+<?php
 
-// crear datos
+ include('../config/database.php');
+
 $fname = $_POST['f_name'];
-$Lname = $_POST['l_name'];
+$lname = $_POST['l_name'];
 $email = $_POST['e_mail'];
 $passwd = $_POST['passw'];
 
+//$enc_pass = md5($passwd);
+$enc_pass = sha1($passwd);
 
-$sql = "INSERT INTO users (firstname, lastname, email, pasword)
-  VALUES('$fname','$Lname','$email','$passwd')
+$sql_email_exist = " SELECT COUNT(email) as total FROM users WHERE email = '$email' LIMIT 1";
+$res = pg_query($conn, $sql_email_exist );
+
+if($res){
+    $row = pg_fetch_assoc($res);
+    if($row['total'] > 0){
+        echo "Email already exists";
+        
+    }else{
+        $sql = "INSERT INTO users (firstname, lastname, email, password)
+        values('$fname','$lname','$email','$enc_pass')
+
 
 ";
 
 $res = pg_query($conn, $sql);
 if($res){
-    echo "users has been created succesfully";
+    echo "user has been created succesfully";
+}else{
+    echo "Error";
 }
-   else{
-    echo "error";
-   }
-
-
+}
+}
 ?>
