0f2898a596d9d0a79ea2b85dbe26c3297ccf2160
Juan <juan04-bravo08@hotmail.com>
2025-04-03 11:19:16 -0500

    signin

---
+++ src/signin.php
@@ -0,0 +1,35 @@
+<?
+include('../config/database.php');
+$email = $_POST['e_mail'];
+$passwd = $_POST['passw'];
+
+$sql = "
+
+SELECT
+--id,
+--email,
+--password,
+
+COUNT(id) as total
+FROM
+ users
+ WHERE
+ email = '$email' and
+ password = '$passw' and
+ status = true
+ GROUP BY
+ id;
+
+";
+$res = pg_query($conn, $sql );
+
+if($res){
+    $row = pg_fetch_assoc($res);
+    if($row['total'] > 0){
+        echo "login ok";
+        
+    }else{ 
+        echo "login failed";
+    }
+}
+?>
